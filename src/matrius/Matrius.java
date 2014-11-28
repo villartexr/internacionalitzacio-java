badaf69b4a9f1000677019af8fca120840508946
GenisVillarte <villartexr@gmail.com>
2014-11-28 10:35:19 +0100

    Final project

--- src/matrius/Matrius.java
+++ src/matrius/Matrius.java
@@ -20,21 +20,31 @@ public class Matrius {
     public static void main(String[] args) {
         final int FILAS = 5, COLUMNAS = 4;
         Scanner sc = new Scanner(System.in);
+        Scanner idiom = new Scanner(System.in);
         int i, j, mayor, menor;
         int filaMayor, filaMenor, colMayor, colMenor;
         int[][] A = new int[FILAS][COLUMNAS];
-        System.out.println("Bienvenido a nuestro programa");
-        System.out.println("Lectura de elementos de la matriz: ");
+        System.out.println("Idioma? ca (catala), es (español), en (english)  :  ");
+        String idi = idiom.next();
+        if(!idi.equals("ca") && !idi.equals("es") && !idi.equals("en")){
+            System.out.println("English Predeterminated");
+            idi = "en";
+        
+        }
+        String idioma = ("matrius/"+idi);
+        //System.out.println("idioma = " + idioma);
+        System.out.println(java.util.ResourceBundle.getBundle(idioma).getString("BIENVENIDO A NUESTRO PROGRAMA"));
+        System.out.println(java.util.ResourceBundle.getBundle(idioma).getString("LECTURA DE ELEMENTOS DE LA MATRIZ: "));
         for (i = 0; i < FILAS; i++) {
             for (j = 0; j < COLUMNAS; j++) {
-                System.out.print("Introduce el elemento A[" + i + "][" + j + "]= ");
+                System.out.print(java.text.MessageFormat.format(java.util.ResourceBundle.getBundle(idioma).getString("INTRODUCE EL ELEMENTO A[{0}][{1}]= "), new Object[] {i, j}));
                 A[i][j] = sc.nextInt();
             }
         }
-        System.out.println("valores introducidos:");
+        System.out.println(java.util.ResourceBundle.getBundle(idioma).getString("VALORES INTRODUCIDOS:"));
         for (i = 0; i < A.length; i++) { 
             for (j = 0; j < A[i].length; j++) {
-                System.out.print(A[i][j] + " ");
+                System.out.print(A[i][j] + java.util.ResourceBundle.getBundle(idioma).getString(" "));
             }
             System.out.println();
         }
@@ -54,11 +64,11 @@ public class Matrius {
                 }
             }           
         }
-        System.out.print("Elemento mayor: " + mayor);
-        System.out.println(" Fila: "+ filaMayor + " Columna: " + colMayor);
-        System.out.print("Elemento menor: " + menor);
-        System.out.println(" Fila: "+ filaMenor + " Columna: " + colMenor);
+        System.out.print(java.text.MessageFormat.format(java.util.ResourceBundle.getBundle(idioma).getString("ELEMENTO MAYOR: {0}"), new Object[] {mayor}));
+        System.out.println(java.util.ResourceBundle.getBundle(idioma).getString(" FILA: ")+ filaMayor + java.text.MessageFormat.format(java.util.ResourceBundle.getBundle(idioma).getString(" COLUMNA: {0}"), new Object[] {colMayor}));
+        System.out.print(java.text.MessageFormat.format(java.util.ResourceBundle.getBundle(idioma).getString("ELEMENTO MENOR: {0}"), new Object[] {menor}));
+        System.out.println(java.util.ResourceBundle.getBundle(idioma).getString(" FILA: ")+ filaMenor + java.text.MessageFormat.format(java.util.ResourceBundle.getBundle(idioma).getString(" COLUMNA: {0}"), new Object[] {colMenor}));
     }
-    
-    
 }
+    
+
